339caedcabffc19cf5de4bc453eea42e2246958b
gdon <139358349+gabrieldonlopes@users.noreply.github.com>
2025-08-28 21:53:30 -0300

    Delete codigo directory

--- codigo/dependencies.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
-
-# nota: esse arquivo serve para reduzir a possibilidade
-# de imports circulares
